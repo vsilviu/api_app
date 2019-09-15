38fc75c282c47ea08cdd6ac365ffbee913230ce4
vborcea <virgil.borcea@esolutions.ro>
2019-09-15 16:52:33 +0300

    * small adjustments

--- src/main/java/ro/apisec/ms/demo/controller/Mappings.java
+++ src/main/java/ro/apisec/ms/demo/controller/Mappings.java
@@ -1,5 +1,6 @@
 package ro.apisec.ms.demo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -47,7 +48,7 @@ public class Mappings {
     }
 
     @GetMapping("/methodAuthorization")
-//    @PreAuthorize("hasAuthority('SOME_UNDEFINED_AUTHORITY')")
+    @PreAuthorize("hasAuthority('SOME_UNDEFINED_AUTHORITY')")
     public String authorizationOnMethod() {
         return "/error/403";
     }
